7dbe282368565098a2437f64d0e59c2e8b45bebe
David Giardi <david.giardi.eng@gmail.com>
2019-01-27 03:51:25 +0100

    Add door sound

--- game_objects/mechanics/LateralDoor.gd
+++ game_objects/mechanics/LateralDoor.gd
@@ -8,9 +8,11 @@ func _ready():
         $Pivot.scale.x = -1
 
 func _on_Area2D_body_entered(body):
+    $DoorSound.play()
     $Pivot/Open.show()
     $Pivot/Close.hide()
 
 func _on_Area2D_body_exited(body):
+    $DoorSoundClose.play()
     $Pivot/Close.show()
     $Pivot/Open.hide()
