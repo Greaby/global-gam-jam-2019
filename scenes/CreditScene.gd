8c9874a95fbdfa22954fe997325293f1bd063dd0
Greaby <contact@greaby.com>
2019-01-27 16:42:59 +0100

    add music to end screen

--- scenes/CreditScene.gd
+++ scenes/CreditScene.gd
@@ -6,7 +6,8 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-    pass # Replace with function body.
+    GameSingleton.play_overworld_music()
+    GameSingleton.stop_title_music()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
@@ -17,3 +18,6 @@ func _physics_process(delta):
         $HUD/AnimationPlayer.play("MovingCar")
     if not $HUD/AnimationPlayer2.is_playing():
          $HUD/AnimationPlayer2.play("CarBump")
+
+func _on_AnimationPlayer_animation_finished(anim_name):
+    get_tree().change_scene("res://scenes/TitleScreen.tscn")
