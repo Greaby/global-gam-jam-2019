4c7aa4fb12dc42de1492c450fce56e0ad52d4147
David Giardi <david.giardi.eng@gmail.com>
2019-01-27 01:23:25 +0100

    Put title screen first

--- scenes/TitleScreen.gd
+++ scenes/TitleScreen.gd
@@ -4,6 +4,8 @@ extends Control
 # var a = 2
 # var b = "text"
 
+var anim_done = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
     pass # Replace with function body.
@@ -16,3 +18,24 @@ func _ready():
 func _on_AnimationPlayer_animation_finished(anim_name):
     if anim_name == "enter":
         $AnimationPlayer.play("press_start")
+        anim_done = true
+
+func _input(event):
+    
+    if event.is_action_pressed("ui_accept"):
+        print("accept")
+        
+        if anim_done:
+            go_to_next_scene()
+        else:
+            #End frame
+            $AnimationPlayer.seek(2, true)
+            $AnimationPlayer.play("press_start")
+            anim_done = true
+            
+        
+func go_to_next_scene():
+    get_tree().change_scene("res://scenes/GameScene.tscn")
+
+    
+    
